f1e58e182f7be3b4e607cd51c6753f51024ddb5b
eqMoura01 <victorlvmora@gmail.com>
2023-11-15 19:48:52 -0300

    Adicionado validação NotNull em alguns parametros.

--- back-end/src/main/java/com/laboratorio/provajava/model/Paciente.java
+++ back-end/src/main/java/com/laboratorio/provajava/model/Paciente.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,22 +24,37 @@ public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String nome;
+    @NotNull
     private String cpf;
+    @NotNull
     private String rg;
+    @NotNull
     @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate data_nasc;
+    @NotNull
     private String sexo;
+    @NotNull
     private String mae;
     private String pai;
+    @NotNull
     private String cep;
+    @NotNull
     private String endereco;
+    @NotNull
     private int numero;
+    @NotNull
     private String cidade;
+    @NotNull
     private String estado;
     private String telefone_fixo;
+    @NotNull
     private String celular;
+    @NotNull
     private Double altura;
+    @NotNull
     private Double peso;
+    @NotNull
     private String tipo_sanguineo;
 }
